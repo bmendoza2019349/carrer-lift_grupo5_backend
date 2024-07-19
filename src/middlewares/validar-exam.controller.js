a40552a31012066070337a26725f5be17dc4cd14
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-07-19 10:23:22 -0600

    Correccion

--- src/middlewares/validar-exam.controller.js
+++ src/middlewares/validar-exam.controller.js
@@ -1,6 +1,5 @@
 import Exam from "./exam.model.js";
 import Module from '../course/course.model.js';
-import { ForbiddenError } from '../errors/customErrors.js';
 
 export const examPermissionMiddleware = async (req, res, next) => {
     try {
@@ -10,33 +9,30 @@ export const examPermissionMiddleware = async (req, res, next) => {
 
         const exam = await Exam.findById(examId);
         if (!exam) {
-            return res.status(404).send("Examen no encontrado");
+            return res.status(404).json({ message: "Examen no encontrado" });
         }
 
-        // Si el usuario es profesor, verifique si creó el examen
+        // Si el usuario es un profesor, verifica si creó el examen
         if (userRole === 'professor') {
             if (exam.createBy !== userEmail) {
-                throw new ForbiddenError('No tienes permiso para acceder a este examen');
+                return res.status(403).json({ message: 'No tienes permiso para acceder a este examen' });
             }
         } 
-        // Si el usuario es estudiante, verifique si el examen es en alguno de sus módulos.
+        // Si el usuario es un estudiante, verifica si el examen está en uno de sus módulos
         else if (userRole === 'student') {
             const module = await Module.findOne({ exams: examId, students: req.user.id });
             if (!module) {
-                throw new ForbiddenError('No tienes permiso para acceder a este examen');
+                return res.status(403).json({ message: 'No tienes permiso para acceder a este examen' });
             }
         } 
-        // Si el usuario no es profesor ni estudiante, negar el acceso
+        // Si el usuario no es ni profesor ni estudiante, deniega el acceso
         else {
-            throw new ForbiddenError('Rol de usuario no autorizado');
+            return res.status(403).json({ message: 'Rol de usuario no autorizado' });
         }
 
-        // Si se pasan todas las comprobaciones, continúe con el siguiente middleware o controlador de ruta
+        // Si todas las verificaciones pasan, procede al siguiente middleware o manejador de ruta
         next();
     } catch (error) {
-        if (error instanceof ForbiddenError) {
-            return res.status(403).json({ message: error.message });
-        }
         return res.status(500).json({ message: 'Error al verificar permisos del examen' });
     }
 };
